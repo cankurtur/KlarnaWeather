6ca86795499e8bb8d7fc5d7875895acc3fc1d344
cankurtur <cankurtur@gmail.com>
2024-07-28 01:20:48 +0200

    create weather view and home view

---
+++ KlarnaWeather/KlarnaWeather/Views/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  KlarnaWeather
+//
+//  Created by Can Kurtur on 27.07.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        WeatherView()
+    }
+
+}
+
+#Preview {
+    HomeView()
+}
