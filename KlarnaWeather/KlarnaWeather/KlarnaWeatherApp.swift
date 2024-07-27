a1101cb7487b0137c3fd00438221000dae1bb0fe
cankurtur <cankurtur@gmail.com>
2024-07-27 18:23:54 +0200

    init project

---
+++ KlarnaWeather/KlarnaWeather/KlarnaWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlarnaWeatherApp.swift
+//  KlarnaWeather
+//
+//  Created by Can Kurtur on 27.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct KlarnaWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
