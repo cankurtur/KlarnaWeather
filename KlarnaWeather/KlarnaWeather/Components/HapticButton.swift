d0d4ec69558c589658713fbd4926c494162995f2
Can Kürtür <51170501+cankurtur@users.noreply.github.com>
2024-07-28 18:33:16 +0200

    Feature/location manager (#6)
    * create app button
    
    * create locaiton manager

---
+++ KlarnaWeather/KlarnaWeather/Components/HapticButton.swift
@@ -0,0 +1,25 @@
+//
+//  HapticButton.swift
+//  KlarnaWeather
+//
+//  Created by Can Kurtur on 28.07.2024.
+//
+
+import SwiftUI
+
+struct HapticButton<Label: View>: View {
+    var feedbackStyle: UIImpactFeedbackGenerator.FeedbackStyle = .light
+    var action: () -> Void
+    @ViewBuilder
+    var label: Label
+    
+    var body: some View {
+        Button(action: {
+            let generator = UIImpactFeedbackGenerator(style: feedbackStyle)
+            generator.impactOccurred()
+            action()
+        }, label: {
+            label
+        })
+    }
+}
