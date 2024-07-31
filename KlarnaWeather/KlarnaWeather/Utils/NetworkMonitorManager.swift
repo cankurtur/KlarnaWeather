80065b9edfc450f1467d541d1a01a88bbb6eeeed
cankurtur <cankurtur@gmail.com>
2024-08-01 01:28:53 +0200

    refactor managers

--- KlarnaWeather/KlarnaWeather/Utils/NetworkMonitorManager.swift
+++ KlarnaWeather/KlarnaWeather/Utils/NetworkMonitorManager.swift
@@ -8,30 +8,47 @@
 import Foundation
 import Network
 
+// MARK: - NetworkMonitorManagerInterface
+
+/// Protocol defining the interface for network monitoring.
+protocol NetworkMonitorManagerInterface {
+    var isReachable: Published<Bool>.Publisher { get }
+    func startMonitoring()
+    func cancel()
+}
+
 // MARK: - NetworkMonitorManager
 
+/// Singleton class responsible for monitoring network connectivity.
 final class NetworkMonitorManager: ObservableObject {
+    static let shared = NetworkMonitorManager()
+    
     private let monitor: NWPathMonitor = NWPathMonitor()
     private let queue = DispatchQueue(label: "NetworkMonitor")
-    @Published var isReachable: Bool = false
+    @Published private var hasConnection: Bool = false
 
-    init() {
-        startMonitoring()
-    }
-    
+    private init() { }
+}
+
+// MARK: - NetworkMonitorManagerInterface
+
+extension NetworkMonitorManager: NetworkMonitorManagerInterface {
+    var isReachable: Published<Bool>.Publisher { $hasConnection }
+    /// Starts monitoring network connectivity.
     func startMonitoring() {
         monitor.pathUpdateHandler = { [weak self] monitorPath in
             guard let self = self else { return }
             
             Task {
                 await MainActor.run {
-                    self.isReachable = monitorPath.status == .satisfied
+                    self.hasConnection = monitorPath.status == .satisfied
                 }
             }
         }
         monitor.start(queue: queue)
     }
     
+    /// Stops monitoring network connectivity.
     func cancel() {
         monitor.cancel()
     }
